02d1472211aa1a5f64e49339ebbde3890e051575
Theo Hiraclides <theo.hiraclides@gmail.com>
2022-11-06 15:05:12 +0100

    Epic Hack Button

---
+++ scripts/hack_button.gd
@@ -0,0 +1,30 @@
+extends Node
+
+export var progress_speed = 50
+export(Color) var done_color
+
+var progress = 0
+
+var isButtonDown = false
+var isDone = false
+
+onready var progress_bar = $ProgressBar
+onready var button = $Button
+
+func _process(delta: float) -> void:
+	if not isDone and isButtonDown:
+		progress += progress_speed * delta
+		if progress >= 100:
+			progress = 100
+			isDone = true
+			button.disabled = true
+			progress_bar.add_color_override("font_color", done_color)
+		progress_bar.value = progress
+
+
+func _on_Button_button_down() -> void:
+	isButtonDown = true
+
+
+func _on_Button_button_up() -> void:
+	isButtonDown = false
